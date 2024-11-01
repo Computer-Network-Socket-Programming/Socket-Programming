cb643a67b01c0d43b450ad37448a706a4c45c095
dochanii <dochaii@github.com>
2024-11-01 16:23:02 +0900

    Fix: 새로운 판넬을 적용시키기 위한 레이아웃 수정

--- SocketProgramming/src/main/Main.java
+++ SocketProgramming/src/main/Main.java
@@ -1,24 +1,37 @@
 package main;
 
 import view.MailDetailPanel;
+import view.MailListTestPanel;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Main extends JFrame {
     public Main() {
-        BorderLayout borderLayout = new BorderLayout();
+
+        setTitle("Mail Application");
         setSize(600, 600);
-
-        MailDetailPanel mailDetailPanel = new MailDetailPanel();
-        add(mailDetailPanel, borderLayout.CENTER);
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel mainPanel = new JPanel(cardLayout);
+
+
+        MailDetailPanel mailDetailPanel = new MailDetailPanel(cardLayout, mainPanel);
+        mainPanel.add(mailDetailPanel, "MailDetailPanel");
+
+
+        MailListTestPanel mailListTestPanel = new MailListTestPanel();
+        mainPanel.add(mailListTestPanel, "MailListPanel");
+
+        cardLayout.show(mainPanel, "MailDetailPanel");
+
+        add(mainPanel);
+
         setVisible(true);
     }
-
 
     public static void main(String[] args) {
         new Main();
