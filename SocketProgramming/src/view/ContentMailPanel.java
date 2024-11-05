d5820be85024276ba547caaeeda52a561d5881cb
dochanii <dochaii@github.com>
2024-11-06 00:42:42 +0900

    refactor: 머지를 위한 코드 정리

--- SocketProgramming/src/view/ContentMailPanel.java
+++ SocketProgramming/src/view/ContentMailPanel.java
@@ -1,17 +1,16 @@
 package view;
 
-import model.DeliverMailDTO;
+import model.MailInfoDTO;
 import model.ReplyMailDTO;
+import model.DeliverMailDTO;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 public class ContentMailPanel extends JPanel {
-    ArrayList<String> dummydatas = new ArrayList<>();
-    MailInfo mailInfo;
+
     JLabel senderLabel, receiverLabel, dateLabel, subjectLabel, contentLabel;
     JTextField senderAddress, receiverAddress, mailDate, subject;
     JTextArea content;
@@ -21,27 +20,17 @@ public class ContentMailPanel extends JPanel {
     JButton mailDeliverButton;
     BorderLayout borderLayout;
     GridLayout gridLayout;
-
+    private int categoryIndex;
+    private MailInfoDTO mailInfoDTO;
 
     public ContentMailPanel(CardLayout cardLayout, JPanel parentPanel) {
-
-
         borderLayout = new BorderLayout();
-        gridLayout = new GridLayout(0,2);
-
-        dummydatas.add("sender@naver.com");
-        dummydatas.add("receiver@naver.com");
-        dummydatas.add("2024년 10월 30일 00:00");
-        dummydatas.add("메일 제목");
-        dummydatas.add("자니? 자는구나... 잘자.. 자니? 자는구나... 잘자.. 자니? 자는구나... 잘자.. 자니? 자는구나... 잘자.. 자니? 자는구나... 잘자.. 자니? 자는구나... 잘자.. 자니? 자는구나... 잘자..");
-
-        mailInfo = new MailInfo(dummydatas.get(0), dummydatas.get(1), dummydatas.get(2), dummydatas.get(3), dummydatas.get(4));
+        gridLayout = new GridLayout(0, 2);
         setLayout(borderLayout);
-
 
         JPanel mailDetailPanel = new JPanel();
         mailDetailPanel.setLayout(gridLayout);
-        add(mailDetailPanel,BorderLayout.NORTH);
+        add(mailDetailPanel, BorderLayout.NORTH);
 
         subjectLabel = new JLabel("메일 제목:", JLabel.RIGHT);
         senderLabel = new JLabel("보낸 사람:", JLabel.RIGHT);
@@ -49,23 +38,17 @@ public class ContentMailPanel extends JPanel {
         dateLabel = new JLabel("날짜:", JLabel.RIGHT);
         contentLabel = new JLabel("내용", JLabel.LEFT);
 
-
-
         subject = new JTextField(50);
         subject.setEditable(false);
-        subject.setText(mailInfo.getSubject());
 
         senderAddress = new JTextField(20);
         senderAddress.setEditable(false);
-        senderAddress.setText(mailInfo.getSender());
 
         receiverAddress = new JTextField(20);
         receiverAddress.setEditable(false);
-        receiverAddress.setText(mailInfo.getReceiver());
 
         mailDate = new JTextField(20);
         mailDate.setEditable(false);
-        mailDate.setText(mailInfo.getDate());
 
         mailDetailPanel.add(subjectLabel);
         mailDetailPanel.add(subject);
@@ -77,24 +60,22 @@ public class ContentMailPanel extends JPanel {
         mailDetailPanel.add(mailDate);
         mailDetailPanel.add(contentLabel);
 
-
         content = new JTextArea(5, 20);
         content.setEditable(false);
         content.setLineWrap(true);
         content.setWrapStyleWord(true);
-        content.setText(mailInfo.getContent());
 
         contentScroll = new JScrollPane(content);
         contentScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-        add(contentScroll,BorderLayout.CENTER);
+        add(contentScroll, BorderLayout.CENTER);
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(gridLayout);
-        add(buttonPanel,BorderLayout.SOUTH);
+        add(buttonPanel, BorderLayout.SOUTH);
 
         mailListButton = new JButton("목록으로 돌아가기");
-        buttonPanel.add(mailListButton,gridLayout);
+        buttonPanel.add(mailListButton);
         mailListButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -103,55 +84,65 @@ public class ContentMailPanel extends JPanel {
                 }
             }
         });
-        replyMailButton = new JButton("답장");
-        buttonPanel.add(replyMailButton,gridLayout);
-        replyMailButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == replyMailButton) {
-                    String sender = mailInfo.getSender();
-                    String receiver = mailInfo.getReceiver();
-                    String content = mailInfo.getContent();
-                    if (sender == null || sender.isEmpty()) {
-                        JOptionPane.showMessageDialog(null, "보낸 사람 주소가 유효하지 않습니다.");
-                        return;
-                    }
-                    if (receiver == null || receiver.isEmpty()) {
-                        JOptionPane.showMessageDialog(null,"받는 사람 주소가 유효하지 않습니다.");
-                        return;
-                    }
-                    if (content == null || content.isEmpty()) {
-                        JOptionPane.showMessageDialog(null, "내용이 존재하지 않습니다.");
-                    }
 
-                    ReplyMailDTO replyMailDTO = new ReplyMailDTO("Re:   "+ receiver, "-----Original Message-----\n\n" + content +"\n\n");
-                    new SenderFrame("dmlwhd010@naver.com","yuneui1523",replyMailDTO);
-                }
-            }
-        });
+        switch (categoryIndex) {
+            case 0: // 받은메일함일 때
 
-        mailDeliverButton = new JButton("전달");
-        buttonPanel.add(mailDeliverButton,gridLayout);
-        mailDeliverButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == mailDeliverButton) {
-                    String sender = mailInfo.getSender();
-                    String subject = mailInfo.getSubject();
-                    String content = mailInfo.getContent();
+                replyMailButton = new JButton("답장");
+                buttonPanel.add(replyMailButton);
+                replyMailButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource() == replyMailButton) {
+                            ReplyMailDTO replyMailDTO = new ReplyMailDTO(
+                                    mailInfoDTO.receiver(),
+                                    "-----Original Message-----\n\n" + mailInfoDTO.content() + "\n\n"
+                            );
+                            new SenderFrame("메일주소", "비밀번호", replyMailDTO);
+                        }
+                    }
+                });
 
-                    if (subject == null || subject.isEmpty()) {
-                        JOptionPane.showMessageDialog(null,"제목이 유효하지 않습니다.");
+
+            case 1: // 보낸메일함일 때 전달 버튼만 표시
+                mailDeliverButton = new JButton("전달");
+                buttonPanel.add(mailDeliverButton);
+                mailDeliverButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource() == mailDeliverButton) {
+                            if (mailInfoDTO.subject() == null || mailInfoDTO.subject().isEmpty()) {
+                                JOptionPane.showMessageDialog(null, "제목이 유효하지 않습니다.");
+                                return;
+                            }
+                            if (mailInfoDTO.content() == null || mailInfoDTO.content().isEmpty()) {
+                                JOptionPane.showMessageDialog(null, "내용이 존재하지 않습니다.");
+                                return;
+                            }
+                            DeliverMailDTO deliverMailDTO = new DeliverMailDTO(
+                                    mailInfoDTO.subject(),
+                                    mailInfoDTO.content()
+                            );
+                            new SenderFrame("메일 주소", "비밀번호", deliverMailDTO);
+                        }
                     }
-                    if (content == null || content.isEmpty()) {
-                        JOptionPane.showMessageDialog(null,"내용이 존재하지 않습니다.");
-                    }
-                    DeliverMailDTO deliverMailDTO = new DeliverMailDTO(subject,content);
-                    new SenderFrame("dmlwhd010@naver.com","yuneui1523",deliverMailDTO);
-                }
-            }
-        });
+                });
+                break;
+
+            default:
+                break;
+        }
     }
 
+    public void updateValue(String[] value, int index) {
+        this.mailInfoDTO = new MailInfoDTO(value[0], value[1], value[2], value[3], value[4]);
+        this.categoryIndex = index;
+
+        senderAddress.setText(mailInfoDTO.sender());
+        receiverAddress.setText(mailInfoDTO.receiver());
+        subject.setText(mailInfoDTO.subject());
+        mailDate.setText(mailInfoDTO.date());
+        content.setText(mailInfoDTO.content());
+    }
 
 }
